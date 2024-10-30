28c138c3f5aad2345eb7fe597caee79bc7394260
Oliver Vamp <70987999+horewoo@users.noreply.github.com>
2024-10-30 00:12:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ client.once('ready', () => {
             player.play(resource);
 
             client.user.setPresence({
-                activities: [{ name: `♡ ${path.basename(initialFile, '.mp3')}`, type: ActivityType.Listening }],
+                activities: [{ name: `INICIANDO: ${path.basename(initialFile, '.mp3')}`, type: ActivityType.Listening }],
                 status: 'dnd',
             });
         };
